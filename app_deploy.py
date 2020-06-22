5f9c929ec9971113099b73144681e00902d63c10
sumukha <ar-sumukha.m@rakuten.com>
2020-06-23 00:34:52 +0530

    chore(app): showing percentange and basic app is built

--- app_deploy.py
+++ app_deploy.py
@@ -1,20 +1,21 @@
 from flask import Flask, redirect, url_for, request, render_template
 from sklearn.externals import joblib
+import decimal
 import json
 
 app = Flask(__name__)
 
-filename = './liver_SVM.joblib.pkl'
+filename = './liver_svc_proba.joblib.pkl'
 
-svc_load = joblib.load(filename)
+svc_load = joblib.load(open(filename,'rb'))
 
 @app.route('/',methods=["GET"])
 def home_func():
-	return render_template("homepage.html")
+	return render_template("main.html")
 	
 
 
-@app.route('/liver',methods=["POST"])
+@app.route('/liver',methods=["POST", "GET"])
 def sendLiverDetails():
 	if request.method=="POST":
 		liverDetails = (request.get_json(force=True));
@@ -37,11 +38,26 @@ def sendLiverDetails():
 			if liverDetails[i] == "":
 				return "Please enter " + i + " details"
 
-		res = svc_load.predict([X])
-   		if res[0]==1:
-				return "your liver is Damaged"
-		else:
-				return "your liver is functioning well"
+		# res = svc_load.predict([X])
+		res = svc_load.predict_proba([X])
+		y= round(decimal.Decimal(res[0][0])*100,2)
+		stng = "There are " + str(y) +  "%" +" chance of Liver Damage"
+		return stng
+   		# if res[0]==1:
+		# 		return "your liver is Damaged"
+		# else:
+		# 		return "your liver is functioning well"
+	if request.method == "GET":
+		return render_template("liver.html")
+			
+# @app.route('/liver',methods=["GET"])
+# def liver_page():
+	
+
+@app.route('/help',methods=["GET"])
+def liver_page():
+	return render_template("help.html")
+
 
 if __name__ == '__main__':
    app.run()
